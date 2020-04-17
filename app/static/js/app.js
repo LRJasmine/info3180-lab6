968f44fd70b8de9a549f1fff27c42699efaddc15
Ubuntu <ubuntu@ip-172-31-43-0.ec2.internal>
2020-04-17 19:12:37 +0000

    new Vue component

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -26,6 +26,29 @@ Vue.component('app-header', {
     }
 });
 
+Vue.component('news-list', {
+    template: `
+    <div class="news">
+        <h2>News</h2>
+        <ul class="news__list">
+            <li class="news__item">News item 1</li>
+            <li class="news__item">News item 2</li>
+            <li class="news__item">News item 3</li>
+        </ul>
+    </div>
+    `,
+     created: function() {
+         fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=<your-api-key>')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            });
+     } 
+});
+
+
 Vue.component('app-footer', {
     template: `
         <footer>
